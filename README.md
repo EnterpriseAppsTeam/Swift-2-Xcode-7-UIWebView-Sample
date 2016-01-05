0d304f0cf0cfe81430bf4558c9adb0a9ee28c6f2
mstcode <info@mstcode.com>
2016-01-05 11:16:56 +0200

    First Commit

---
+++ README.md
@@ -0,0 +1,11 @@
+##Swift 2 UIWebView Tutorial
+
+Source code on adding UIWebView in Swift 2 Using Xcode 7.2
+
+Tutorial: https://youtu.be/wUCta6wAECU
+
+Check out our Website: http://www.mstcode.com
+
+Follow us on Twitter: https://twitter.com/mstcode_
+
+Like us on Facebook: http://on.fb.me/1OcsVuX
---
+++ UIWebView/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  UIWebView
+//
+//  Created by mstcode on 04/01/16.
+//  Copyright © 2016 mstcode. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var webView: UIWebView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        let url = NSURL(string: "http://www.mstcode.com")
+        webView.loadRequest(NSURLRequest(URL: url!))
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
